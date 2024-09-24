967d38d05313d61b806f476bb2c8dfb631099fb5
Rothman <rothmanalvarado@example.com>
2024-09-24 17:55:10 -0600

    creación de dir y archivo

---
+++ script.js
@@ -0,0 +1,31 @@
+//CLASE 24/09/2024
+//NOMBRE:
+
+/*
+LAS VARIABLES GUARDAN INFORMACIÓN QUE PUEDES REUTILIZAR
+*/
+
+//Declaracion de variables 
+
+let nombre = 'Pedro'; //usa let declar variables que pueden cambiar su valor
+const PI = 3.1416;
+var edad = 25;
+
+//operadores
+let a = 5;
+let b = 2;
+let suma = a + b; //7
+let resta = a - b; //3
+let ultiplicacion = a * b; //10
+let division = a / b; //2.5
+
+//operadores de comparación 
+a > b; //true
+a < b; //false 
+a == b; //false
+a === b; //false (compara valor)
+
+
+
+
+
